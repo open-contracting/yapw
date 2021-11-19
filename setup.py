7f0dc232b637d0d558520fa146ea8d910ab6d607
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-11-18 19:32:23 -0500

    ci: Drop support for Python 3.6 and 3.7

--- setup.py
+++ setup.py
@@ -35,8 +35,6 @@ setup(
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
