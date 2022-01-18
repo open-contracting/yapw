d30b8eb92f1488b805c0fe60d7bde8ebb226b266
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-01-18 15:35:05 -0500

    build: Iterate the version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as f:
 
 setup(
     name="yapw",
-    version="0.0.6",
+    version="0.0.7",
     author="Open Contracting Partnership",
     author_email="data@open-contracting.org",
     url="https://github.com/open-contracting/yapw",
