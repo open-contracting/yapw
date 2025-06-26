3079e16c561b39492dcc1311b1d0e42ab007a138
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-06-26 16:07:51 -0400

    docs: Use str with sys.path to avoid mypy error

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, Path("..").resolve())
+sys.path.insert(0, str(Path(__file__).parent.parent))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
